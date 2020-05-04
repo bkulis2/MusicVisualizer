94a0fa0659158cfe671580fbddcbd523cc3f0437
bkulis2 <bkulis2@illinois.edu>
2020-05-04 18:28:38 -0500

    Moved drawing logic into Visualizer interface

--- src/InteractiveSineVisualizer.cpp
+++ src/InteractiveSineVisualizer.cpp
@@ -22,40 +22,14 @@ namespace visualizer {
 			ci::audio::Output::play(ci::audio::createCallback(this, &InteractiveSineVisualizer::BuildSine));
 			sine_playing_ = true;
 		}
-
-		if (fft_) {
-			float * frequency_data = fft_->getAmplitude();
-			float * time_data = fft_->getData();
-			int32_t bin_size = fft_->getBinSize();
-
-			float scale = ((float) ci::app::getWindowWidth() - 20.0f) / (float) bin_size;
-			float window_height = (float) ci::app::getWindowHeight();
-
-			ci::PolyLine<ci::Vec2f> frequency_line;
-			ci::PolyLine<ci::Vec2f> time_line;
-
-			for (int32_t i = 0; i < bin_size; i++) {
-				float log_size = ci::math<float>::log((float)bin_size);
-				float x = (float)((ci::math<float>::log((float)i) / log_size) * (float)bin_size);
-				float y = ci::math<float>::clamp(frequency_data[i] * (x / (float)bin_size)
-					* (ci::math<float>::log(((float)bin_size - (float)i))), 0.0f, 2.0f);
-
-				frequency_line.push_back(ci::Vec2f(x * scale + 10.0f, -y
-					* (window_height - 20.0f) * 0.25f + (window_height - 10.0f)));
-				time_line.push_back(ci::Vec2f((float)i * scale + 10.0f, time_data[i]
-					* (window_height - 20.0f) * 0.25f + (window_height * 0.25f + 10.0f)));
-			}
-
-			ci::gl::draw(frequency_line);
-			ci::gl::draw(time_line);
-		}
+		InteractiveSineVisualizer::DrawVisualizerLines();
 	}
 
 	void InteractiveSineVisualizer::mouseMove(ci::app::MouseEvent event) {
 		amplitude_ = 1.0f - event.getY() / (float) ci::app::getWindowHeight();
-		double width = (double) ci::app::getWindowWidth();
-		double x = width - (double)event.getX();
-		float position = (float)((log(width) - log(x)) / log(width));
+		double x = (double)ci::app::getWindowWidth() - (double)event.getX();
+		float position = (float)((log((double)ci::app::getWindowWidth()) - log(x))
+			/ log((double)ci::app::getWindowWidth()));
 		frequency_target_ = ci::math<float>::clamp(max_frequency_
 			* position, min_frequency_, max_frequency_);
 		amplitude_ = ci::math<float>::clamp(amplitude_ * (1.0f - position), 0.05f, 1.0f);
--- src/Visualizer.h
+++ src/Visualizer.h
@@ -12,6 +12,34 @@ namespace visualizer {
 		virtual void draw() = 0;
 
 	protected:
+		void DrawVisualizerLines() {
+			if (fft_) {
+				float * frequency_data = fft_->getAmplitude();
+				float * time_data = fft_->getData();
+				int32_t bin_size = fft_->getBinSize();
+
+				float scale = ((float)ci::app::getWindowWidth() - 20.0f) / (float)bin_size;
+				float window_height = (float)ci::app::getWindowHeight();
+
+				ci::PolyLine<ci::Vec2f> frequency_line;
+				ci::PolyLine<ci::Vec2f> time_line;
+
+				for (int32_t i = 0; i < bin_size; i++) {
+					float log_size = ci::math<float>::log((float)bin_size);
+					float x = (float)((ci::math<float>::log((float)i) / log_size) * (float)bin_size);
+					float y = ci::math<float>::clamp(frequency_data[i] * (x / (float)bin_size)
+						* (ci::math<float>::log(((float)bin_size - (float)i))), 0.0f, 2.0f);
+
+					frequency_line.push_back(ci::Vec2f(x * scale + 10.0f, -y
+						* (window_height - 20.0f) * 0.25f + (window_height - 10.0f)));
+					time_line.push_back(ci::Vec2f((float)i * scale + 10.0f, time_data[i]
+						* (window_height - 20.0f) * 0.25f + (window_height * 0.25f + 10.0f)));
+				}
+
+				ci::gl::draw(frequency_line);
+				ci::gl::draw(time_line);
+			}
+		}
 		KissRef fft_;
 	};
 } //namespace visualizer
