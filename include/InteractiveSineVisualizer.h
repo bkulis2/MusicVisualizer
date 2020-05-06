f35813f745745a8fb14b6db3cb13972ddd7643a8
bkulis2 <bkulis2@illinois.edu>
2020-05-06 16:05:35 -0500

    Fixed magic numbers

--- include/InteractiveSineVisualizer.h
+++ include/InteractiveSineVisualizer.h
@@ -1,17 +1,24 @@
 // Copyright (c) 2020 [Benjamin Kulis]. All rights reserved.
 #pragma once
 #include <cinder/app/AppBasic.h>
-#include <cinder/audio/Output.h>
 #include <cinder/audio/Callback.h>
-#include <cinder/CinderMath.h>
 
-#include "KissFFT.h"
 #include "Visualizer.h"
 
 #ifndef INTERACTIVESINEVISUALIZER_H
 #define INTERACTIVESINEVISUALIZER_H
 
 namespace visualizer {
+	const float kMaxAmplitude = 1.0f;
+	const float kMaxPosition = 1.0f;
+	const float kMinPosition = 0.05f;
+	const float kMaxFrequency = 20000.0f;
+	const float kMinFrequency = 1.0f;
+	const float kSamplingFrequency = 44100.0f;
+	const float kPhaseCorrection = 0.95f;
+	const float kTimeOffset = 0.05f;
+	const float kSineConst = 2.0f * (float)M_PI;
+
 	class InteractiveSineVisualizer : public Visualizer {
 	public:
 		void	setup() override;
@@ -25,8 +32,6 @@ namespace visualizer {
 		float	frequency_target_;
 		float	phase_;
 		float	phase_adjust_;
-		float	max_frequency_;
-		float	min_frequency_;
 		bool    sine_playing_;
 	};
 } //namespace visualizer
--- src/InteractiveSineVisualizer.cpp
+++ src/InteractiveSineVisualizer.cpp
@@ -1,17 +1,11 @@
 // Copyright (c) 2020 [Benjamin Kulis]. All rights reserved.
-#include <cinder/audio/Output.h>
-#include <cinder/audio/Callback.h>
-#include <cinder/CinderMath.h>
 
 #include "InteractiveSineVisualizer.h"
-#include "KissFFT.h"
 
 namespace visualizer {
 
 	void InteractiveSineVisualizer::setup() {
 		amplitude_ = 0.5f;
-		max_frequency_ = 20000.0f;
-		min_frequency_ = 1.0f;
 		frequency_target_ = 0.0f;
 		phase_ = 0.0f;
 		phase_adjust_ = 0.0f;
@@ -27,13 +21,12 @@ namespace visualizer {
 	}
 
 	void InteractiveSineVisualizer::mouseMove(ci::app::MouseEvent event) {
-		amplitude_ = 1.0f - event.getY() / (float) ci::app::getWindowHeight();
+		amplitude_ = kMaxAmplitude - event.getY() / (float) ci::app::getWindowHeight();
 		double x = (double)ci::app::getWindowWidth() - (double)event.getX();
 		float position = (float)((log((double)ci::app::getWindowWidth()) - log(x))
 			/ log((double)ci::app::getWindowWidth()));
-		frequency_target_ = ci::math<float>::clamp(max_frequency_
-			* position, min_frequency_, max_frequency_);
-		amplitude_ = ci::math<float>::clamp(amplitude_ * (1.0f - position), 0.05f, 1.0f);
+		frequency_target_ = ci::math<float>::clamp(kMaxFrequency * position, kMinFrequency, kMaxFrequency);
+		amplitude_ = ci::math<float>::clamp(amplitude_ * (kMaxPosition - position), kMinPosition, kMaxPosition);
 	}
 
 	void InteractiveSineVisualizer::shutdown() {
@@ -43,11 +36,11 @@ namespace visualizer {
 	}
 
 	void InteractiveSineVisualizer::BuildSine(uint64_t sample_offset, uint32_t sample_count, ci::audio::Buffer32f *buffer) {
-		phase_adjust_ = phase_adjust_ * 0.95f + (frequency_target_ / 44100.0f) * 0.05f;
+		phase_adjust_ = phase_adjust_ * kPhaseCorrection + (frequency_target_ / kSamplingFrequency) *  kTimeOffset;
 		for (uint32_t i = 0; i < sample_count; i++) {
 			phase_ += phase_adjust_;
 			phase_ = phase_ - ci::math<float>::floor(phase_);
-			float val = ci::math<float>::sin(phase_ * 2.0f * (float)M_PI) * amplitude_;
+			float val = ci::math<float>::sin(phase_ * kSineConst) * amplitude_;
 			buffer->mData[i * buffer->mNumberChannels] = val;
 			buffer->mData[i * buffer->mNumberChannels + 1] = val;
 		}
